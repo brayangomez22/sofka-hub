c0025cadb13f923ce85616a089497d48738442a4
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-04-16 17:44:29 -0500

    Structure of the finished about section

---
+++ frontend/src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="about">
+            <h2>
+                If you are a lover of the night, this is your place ...
+            </h2>
+            <p>
+                Se denomina retirada romana de África al rescate de la República romana en 255 a. C. de los supervivientes de su derrotada fuerza expedicionaria al África cartaginesa durante la primera guerra púnica. Una gran flota dirigida por Servio Fulvio Petino Nobilior y Marco Emilio Paulo evacuó con éxito los restos de la expedición, y derrotó a una flota cartaginesa en el camino, pero una tormenta la golpeó mientras regresaba, por lo que perdió la mayoría de sus barcos.
+                <br/>
+                <br/>
+                Los romanos habían invadido la patria cartaginesa —en lo que hoy es el noreste de Túnez— en 256 a. C. Después de los éxitos iniciales, habían dejado una fuerza de 15 500 hombres para mantener su alojamiento durante el invierno. Esta fuerza, bajo el mando de Marco Atilio Régulo, sufrió una derrota decisiva en la batalla de Túnez en la primavera de 255 a. C.; Régulo cayó prisionero y los cartagineses sitiaron a 2000 supervivientes en el puerto de Aspis. En respuesta, Roma envió una flota de 390 buques de guerra a rescatarlos y evacuarlos, pero una armada cartaginesa de 200 barcos los interceptó frente al cabo Hermeo —el moderno cabo Bon o Ras ed-Dar—, al norte de Aspis. Los cartagineses sufrieron una derrota, y en consecuencia, los romanos apresaron 214 de sus barcos, junto con sus tripulaciones, y les hundieron otros 16. No se sabe qué pérdidas sufrieron los romanos, si es que hubo alguna; la mayoría de los historiadores modernos asumen que no hubo ninguna.
+                <br/>
+                <br/>
+                Los romanos desembarcaron en Aspis, salieron, dispersaron a los sitiadores, asaltaron el país circundante en busca de comida y se embarcaron de nuevo, hacia Italia. En la esquina sureste de Sicilia, estalló una repentina tormenta de verano que devastó la flota romana. De los 464 buques de guerra que la formaban, 384 se hundieron, al igual que 300 transportes; en total se perdieron más de cien mil hombres. A pesar de las grandes pérdidas de ambos bandos, la guerra continuó durante catorce años más, principalmente en Sicilia o las aguas cercanas, antes de terminar con una victoria romana.
+            </p>
+        </section>
+    )
+}
+
+export default About
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../assets/sass/Home.scss'
 import Header from '../components/Header'
 import Welcome from '../components/Welcome'
+import About from '../components/About'
 
 const Home = () => {
 	return (
 		<div>
 			<Header />
 			<Welcome />
+			<About />
 		</div>
 	)
 }
