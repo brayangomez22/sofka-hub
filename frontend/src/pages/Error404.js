bf135925bd236f396101c4b9f2a503b51668d983
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-04-18 08:50:15 -0500

    Page not found ready to go

---
+++ frontend/src/pages/Error404.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Error404 = () => {
+    return (
+        <div className="container-err">
+            <div id="main">
+                <div id="msg">
+                    Page not found
+                </div>
+                <div id="err">
+                    <div id="f1">4</div>
+                    <div id="circle">
+                        <div id="small-doct">
+
+                        </div>
+                    </div>
+                    <div id="f2">4</div>
+                </div>
+                <div id="info">
+                    We can't seem to find the page you're looking for.
+                </div>
+                <div id="btn-err">
+                    Back to site
+                </div>
+            </div>
+            <div id="ring">
+
+            </div>
+            <div class="dusk" id="d1"></div>
+            <div class="dusk" id="d2"></div>
+            <div class="dusk" id="d3"></div>
+            <div class="dusk" id="d4"></div>
+            <div class="dusk" id="d5"></div>
+            <div class="dusk" id="d6"></div>
+            <div class="dusk" id="d7"></div>
+            <div class="dusk" id="d8"></div>
+            <div class="dusk" id="d9"></div>
+        </div>
+    )
+}
+
+export default Error404;
--- frontend/src/router/Router.js
+++ frontend/src/router/Router.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from '../pages/Home'
 import Social from '../pages/Social'
+import Error404 from '../pages/Error404'
 
 const Router = () => {
 	return (
@@ -9,6 +10,7 @@ const Router = () => {
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route exact path="/social" component={Social} />
+				<Route component={Error404}/>
 			</Switch>
 		</BrowserRouter>
 	)
